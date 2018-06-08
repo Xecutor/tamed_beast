44a67d24ac7e47a9fce5212568fcdaf6164b6fd3
Konstantin Stupnik <konstantin.stupnik@akcp.com>
2018-06-08 15:56:14 +0700

    inc version

--- tamed_beast.cpp
+++ tamed_beast.cpp
@@ -14,7 +14,7 @@ class tamed_beast_app{
 public:
     enum{
         version_major = 0,
-        version_minor = 1,
+        version_minor = 5,
     };
 
     bool init(int argc, const char*const argv[])
